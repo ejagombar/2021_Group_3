ac7020b418c7707480c42db891c669437475084b
ejagombar <ejagombar@gmail.com>
2021-12-07 12:06:17 +0000

    finished GetObjectCountsAndType, GetNumOfCells, GetNumOfVertices

--- Model/Model.cpp
+++ Model/Model.cpp
@@ -13,37 +13,81 @@ using namespace std;
 
 
 //Passing in a file of type ifstream
-void Model::loadModelFile(const string fileName) {
+void Model::loadModelFile(const string fileName)
+{
 	ifstream fileIn;
+
 	fileIn.open(fileName);
-	if (fileIn.is_open()) {
+	if (fileIn.is_open())
+    {
+        std::cout << "FILE OPEN \n";
 
-        string line{};
-        char firstLetter;
-        uint16_t mCount = 0, vCount = 0, cCount = 0;
+        GetObjectCountsAndType(fileIn);
 
-        while (getline(fileIn, line)) {
-            if (fileIn.is_open()) {
+        fileIn.close();
 
-                firstLetter = line.front();
-
-                if (firstLetter == 'V') { vCount += 1; }
-                else if (firstLetter == 'M') { mCount += 1; }
-                else if (firstLetter == 'C') { cCount += 1; }
-                else {
-                    std::cout << "No letters on this line \n";
-                }
-            }
-        }
+        cout << numOfVertices << "\n" << numOfMaterials << "\n" << numOfCells;
+        cout << "\nNum of Tetrahedron: " << cellTypeCount[0] << " Num of Hexahedron: " << cellTypeCount[1] << " Num of Pyramid: " << cellTypeCount[2];
 	}
-	fileIn.close();
+    else
+    {
+        cout << "Failed to open file";
+    }
+    
+
 
 }
 
-void Model::saveModelFile() {
+void Model::saveModelFile()
+{
 
+}
 
+bool Model::GetObjectCountsAndType(ifstream& fileIn)
+{
+    string line{};
+    char firstLetter, cellType;
+    uint16_t vCount = 0, mCount = 0, cCount = 0;
+    uint16_t typeCount[3];
+    typeCount[0] = typeCount[1] = typeCount[2] = 0;
+ 
+    while (getline(fileIn, line))
+    {
+        if (fileIn.is_open() && (!line.empty()))
+        {
+            firstLetter = line.front();
 
+            if (firstLetter == 'v') { vCount += 1; }
+            else if (firstLetter == 'm') { mCount += 1; }
+            else if (firstLetter == 'c')
+            {
+                cCount += 1;
+                cellType = line.at(4);
+
+                switch (cellType) {
+                case 't':
+                    typeCount[0] += 1;
+                    break;
+                case 'h':
+                    typeCount[1] += 1;
+                    break;
+                case 'p':
+                    typeCount[2] += 1;
+                    break;
+                default:
+                    return 0; //error
+                }
+
+            }
+        }
+    }
+
+    memmove(cellTypeCount, typeCount, sizeof(cellTypeCount));
+    numOfVertices = vCount;
+    numOfCells = cCount;
+    numOfMaterials = mCount;
+
+    return 1; //finished with no errors
 }
 
 void Model::CalculateCentre() {
@@ -51,6 +95,16 @@ void Model::CalculateCentre() {
 
 
 
+}
+
+uint16_t Model::GetNumOfVertices()
+{
+    return numOfVertices;
+}
+
+uint16_t Model::GetNumOfCells()
+{
+    return numOfCells;
 }
 
 
@@ -61,5 +115,11 @@ int main()
 	cout << "Hello CMake." << endl;
 	fstream test;
 
+    Model myModel;
+
+    myModel.loadModelFile("exampleModel1.mod");
+
 	return 0;
+
+
 }
