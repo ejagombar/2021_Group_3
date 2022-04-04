69017de0a96c17d3c6eac123759094129d1d0355
ejagombar <77460324+ejagombar@users.noreply.github.com>
2022-04-04 09:41:56 +0000

    Deploying to gh-pages from @ ejagombar/2021_Group_3@ae64487f507c63d1365627d17fa8715c1c2797f8 🚀

--- structcolour.js
+++
@@ -1,6 +0,0 @@
-var structcolour =
-[
-    [ "b", "structcolour.html#a89a348db0cec305ad852512f8b6f661e", null ],
-    [ "g", "structcolour.html#a15b0d2bb568b38317b4e48c72548a94d", null ],
-    [ "r", "structcolour.html#a43919109342a55d3450d8adbc2502e29", null ]
-];
