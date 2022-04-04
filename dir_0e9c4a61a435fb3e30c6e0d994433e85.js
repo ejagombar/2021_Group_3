b6bdc3e0d19b82ae0488183ab13782257a7dba96
ejagombar <ejagombar@gmail.com>
2022-04-04 09:45:33 +0100

    cleaned doxygen

--- dir_0e9c4a61a435fb3e30c6e0d994433e85.js
+++
@@ -1,6 +0,0 @@
-var dir_0e9c4a61a435fb3e30c6e0d994433e85 =
-[
-    [ "filter.h", "filter_8h_source.html", null ],
-    [ "mainwindow.h", "_filter_2mainwindow_8h_source.html", null ],
-    [ "mediator.h", "mediator_8h_source.html", null ]
-];
--- search/files_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['cell_2ecpp_21',['Cell.cpp',['../_cell_8cpp.html',1,'']]]
-];
