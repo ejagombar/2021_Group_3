341b89bc7bd6446aa1797cd0534d0a7f7b0c3087
ejagombar <ejagombar@gmail.com>
2022-04-21 16:22:09 +0100

    added comments to cell.h

--- Current_Working_Program/main.cpp
+++ Current_Working_Program/main.cpp
@@ -7,10 +7,11 @@
 
 int main( int argc, char** argv )
 {
+  QApplication a( argc, argv );
   // needed to ensure appropriate OpenGL context is created for VTK rendering.
   QSurfaceFormat::setDefaultFormat( QVTKOpenGLWidget::defaultFormat() );
 
-  QApplication a( argc, argv );
+
 
   MainWindow window;
   window.show();
